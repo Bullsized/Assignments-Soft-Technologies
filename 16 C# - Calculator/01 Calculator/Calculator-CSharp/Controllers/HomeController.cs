b480786573929ca6b6d3feea6a9b57a7e5defacf
Bullsized <ivanstefanovbg@gmail.com>
2017-08-10 17:51:34 +0300

    Added C# calculator
    soon the Book Library

---
+++ 16 C# - Calculator/01 Calculator/Calculator-CSharp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Calculator_CSharp.Models;
+using System;
+using System.Web.Mvc;
+
+namespace Calculator_CSharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(Calculator calculator)
+        {
+            return View(calculator);
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(Calculator calculator)
+        {
+            calculator.Result = CalculateResult(calculator);
+
+            return RedirectToAction("Index", calculator);
+        }
+
+        private decimal CalculateResult(Calculator calculator)
+        {
+            var result = 0m;
+
+            switch (calculator.Operator)
+            {
+                case "+": result = calculator.LeftOperand + calculator.RightOperand; break;
+                case "-": result = calculator.LeftOperand - calculator.RightOperand; break;
+                case "*": result = calculator.LeftOperand * calculator.RightOperand; break;
+                case "/": result = calculator.LeftOperand / calculator.RightOperand; break;
+                case "^": result = 1;
+                    for (int i = 0; i < calculator.RightOperand; i++)
+                    {
+                        result = calculator.LeftOperand * result;
+                    };
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 16 C# - Calculator/README.md
@@ -0,0 +1,3 @@
+C#: ASP.NET MVC OVERVIEW - EXERCISE
+
+Added the mighty C# Calculator. The .rar file is ready to be presented as a homework. Enjoy.
